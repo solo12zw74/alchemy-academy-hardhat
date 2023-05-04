3fdf2631f9ade10ae3eefa2b5803afba766e59f8
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-05-03 21:31:12 -0600

    Config hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_TESTNET_RPC_URL,
+      accounts: [`${process.env.TESTNET_PRIVATE_KEY}`]
+    }
+  }
 };
 
 export default config;
