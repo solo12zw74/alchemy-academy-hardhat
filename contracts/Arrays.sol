798c7f7f6e5f61c082c3e059e5e5dd75e342f1c9
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-05-16 22:29:11 -0600

    Implement array filtering.
    I don't really like the type conversion
    between Solidity and TS

--- contracts/Arrays.sol
+++ contracts/Arrays.sol
@@ -2,9 +2,19 @@
 pragma solidity ^0.8.9;
 
 contract Arrays {
-    function sum(uint[] memory values) public pure returns (uint result) {
+    uint[] public evenNumbers;
+
+    function sum(uint[] memory values) external pure returns (uint result) {
         for (uint256 index = 0; index < values.length; index++) {
             result += values[index];
+        }
+    }
+
+    function filterEven(uint[] memory values) external {
+        for (uint256 i = 0; i < values.length; i++) {
+            if (values[i] % 2 == 0) {
+                evenNumbers.push(values[i]);
+            }
         }
     }
 }
--- test/Arrays.ts
+++ test/Arrays.ts
@@ -2,11 +2,23 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { Arrays } from "../typechain-types";
+import { BigNumberish, CallOverrides, BigNumber } from "ethers";
+import { PromiseOrValue } from "../typechain-types/common";
 
 async function deployArrays() {
     const Arrays = await ethers.getContractFactory("Arrays");
     const arrays = await Arrays.deploy();
     return { arrays };
+}
+async function getArrayElements(getterFn: { (arg0: PromiseOrValue<BigNumberish>, overrides?: CallOverrides | undefined): Promise<BigNumber>; (arg0: any): any; }) {
+    let vals = [];
+    try {
+        for (let i: number = 0; ; i++) {
+            vals.push(await getterFn(i));
+        }
+    }
+    catch (ex) { }
+    return vals.map(x => Number(x));
 }
 
 describe("Arrays", () => {
@@ -25,5 +37,10 @@ describe("Arrays", () => {
         expect(await _arrays.sum([1, 2, 3, 4, 5])).is.to.eq(15);
         expect(await _arrays.sum([5])).is.to.eq(5);
         expect(await _arrays.sum([1, 1, 1])).is.to.eq(3);
+    });
+
+    it("should filter evens from the input", async () => {
+        await _arrays.filterEven([1, 2, 3, 4, 5, 6]);
+        expect(await getArrayElements(_arrays.evenNumbers)).has.same.members([2, 4, 6].map(v => Number(v)));
     });
 });
