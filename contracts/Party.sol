62dfd4f775bc890ec2617bcb3d4402388f7841cf
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-05-31 22:50:52 -0600

    Implement additional logic for pay bill

--- contracts/Party.sol
+++ contracts/Party.sol
@@ -2,18 +2,40 @@
 pragma solidity ^0.8.9;
 
 contract Party {
-    uint public deposit = 0;
+    uint public depositAmount = 0;
     // keep track of all attendees
-    mapping(address => uint) attendees;
+    mapping(address => bool) attendees;
+    uint contractFunds = 0;
+    address[] attendeesArray;
 
     constructor(uint256 _deposit) {
         require(_deposit > 0);
-        deposit = _deposit;
+        depositAmount = _deposit;
     }
 
     function rsvp() external payable {
-        require(msg.value == deposit);
-        require(attendees[msg.sender] == 0);
-        attendees[msg.sender] = msg.value;
+        require(msg.value == depositAmount);
+        require(attendees[msg.sender] == false);
+        attendees[msg.sender] = true;
+        contractFunds += msg.value;
+        attendeesArray.push(msg.sender);
+    }
+
+    function payBill(address venue, uint totalAmount) external {
+        require(totalAmount <= contractFunds);
+        (bool success, ) = venue.call{value: totalAmount}("");
+        require(success);
+        if (contractFunds == totalAmount) {
+            return;
+        }
+
+        uint totalRemainder = contractFunds - totalAmount;
+        uint attendeesCount = attendeesArray.length;
+        uint remainder = totalRemainder / attendeesCount;
+        for (uint256 index = 0; index < attendeesCount; index++) {
+            address attendee = attendeesArray[index];
+            (bool successRemainderSend, ) = attendee.call{value: remainder}("");
+            require(successRemainderSend);
+        }
     }
 }
