2b34bd4c802c21d3513b3e71a37d2be38a02a2f2
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-06-05 22:03:40 -0600

    Add foundWinner logic

--- contracts/Hackathon.sol
+++ contracts/Hackathon.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "hardhat/console.sol";
+
 contract Hackathon {
     struct Project {
         string title;
@@ -19,5 +21,39 @@ contract Hackathon {
     function rate(uint _idx, uint _rating) external {
         // rates a project by its index
         projects[_idx].ratings.push(_rating);
+    }
+
+    function findWinner() external view returns (Project memory) {
+        uint highestAvgRate = 0;
+        uint highestProjectIndex = 0;
+
+        for (
+            uint projectIndex = 0;
+            projectIndex < projects.length;
+            projectIndex++
+        ) {
+            uint projectRateSum = 0;
+            uint projectRateCounter = projects[projectIndex].ratings.length;
+
+            if (projectRateCounter < 1) {
+                continue;
+            }
+
+            for (
+                uint256 rateIndex = 0;
+                rateIndex < projectRateCounter;
+                rateIndex++
+            ) {
+                projectRateSum += projects[projectIndex].ratings[rateIndex];
+            }
+
+            uint projectAvgRate = projectRateSum / projectRateCounter;
+
+            if (projectAvgRate > highestAvgRate) {
+                highestProjectIndex = projectIndex;
+                highestAvgRate = projectAvgRate;
+            }
+        }
+        return projects[highestProjectIndex];
     }
 }
